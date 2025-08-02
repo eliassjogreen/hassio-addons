f0c69876a15b6f17a3dffc8d8710146711abab90
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2025-08-02 11:16:53 +0200

    new

--- .templates/00-banner.sh
+++ .templates/00-banner.sh
@@ -16,17 +16,9 @@ if ! bashio::supervisor.ping 2> /dev/null; then
         '-----------------------------------------------------------'
     # Use environment variables instead of addon options
     echo "... convert scripts to use environment variables instead of addon options"
-    while IFS= read -r scripts; do
-        [[ "$scripts" == *"00-banner.sh"* ]] && continue
-        sed -i -e 's/bashio::config.has_value[[:space:]]*["'"'"']\([^"'"'"']*\)["'"'"']/[ ! -z "${\1:-}" ]/g' \
-            -e 's/bashio::config.true[[:space:]]*["'"'"']\([^"'"'"']*\)["'"'"']/[ ! -z "${\1:-}" ] \&\& [ "${\1:-}" = "true" ]/g' \
-            -e 's/\$(bashio::config[[:space:]]*["'"'"']\([^"'"'"']*\)["'"'"'])/${\1:-}/g' \
-            -e 's/\$(bashio::addon.port[[:space:]]*["'"'"']\([0-9]*\)["'"'"'])/${\1:-}/g' \
-            -e 's/bashio::config.require.ssl/true/g' \
-            -e 's/\$(bashio::addon.ingress_port)/""/g' \
-            -e 's/\$(bashio::addon.ingress_entry)/""/g' \
-            -e 's/\$(bashio::addon.ip_address)/""/g' "$scripts"
-    done < <(grep -srl "bashio" /etc/cont-init.d /custom-services.d /etc/services.d /etc/s6-overlay)
+    for scripts in $(grep -srl "bashio" /etc/cont-init.d /custom-services.d /etc/services.d /etc/s6-overlay); do
+        sed -i "1a source /.bashio-standalone.sh" "$scripts"
+    done
     exit 0
 fi
 
